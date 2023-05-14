633e3cf988793d0d34b23af76585a3f42369093d
SY <seyav29@gmail.com>
2023-05-14 09:59:33 -0400

    first commit

---
+++ src/__tests__/data/ExternalPropsComponent.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { ExternalPropsComponentProps } from './ExternalPropsComponentProps';
+
+/**
+ * ExternalPropsComponent description
+ */
+export class ExternalPropsComponent extends React.Component<
+  ExternalPropsComponentProps,
+  {}
+> {
+  public render() {
+    return <div>ExternalPropsComponent</div>;
+  }
+}
